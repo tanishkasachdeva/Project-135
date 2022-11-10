5bff79da5085e990d5962c216dc45725e16c1d20
tanishkasachdeva <73342817+tanishkasachdeva@users.noreply.github.com>
2022-11-10 20:12:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status="";
+objects=[];
 
 function setup()
 {
@@ -9,11 +10,44 @@ function setup()
     video.hide();
 }
 
+function draw()
+{
+    image(video,0,0,370,360);
+    if (status!="")
+    {
+
+        for(i=0;i<objects.length;i++)
+        {
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+percent+"%",objects[i].x+15,objects[i].y+15);
+            stroke("#FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+            if(objects[i].label==object_name)
+          {
+            video.stop();
+            objectDetector.detect(gotResults);
+            document.getElementById("status").innerHTML=object_name+" Found";
+            api=window.speechSynthesis();
+            utterThis=SpeechSynthesisUtterance(object_name); 
+            api.speak(utterThis);
+          }
+          else
+          {
+            document.getElementById("status").innerHTML=object_name+" Not Found";
+          }
+        } 
+
+        
+    }
+   
+}
+
 function start()
 {
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
     document.getElementById("status").innerHTML="Status: Detecting Objects";
-    document.getElementById("input_object_name").value;
+    object_name=document.getElementById("input_object_name").value;
 }
 
 function modelLoaded()
@@ -22,7 +56,17 @@ function modelLoaded()
     status=true;
 }
 
-function draw()
+
+
+function gotResults(error,results)
 {
-    image(video,0,0,370,360);
+    if (error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        objects=results;
+    }
 }
